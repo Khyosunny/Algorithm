b52d66f6584ed678969c37a43fd9036914338390
Khyosunny <kimhyo_0218@naver.com>
2021-07-22 00:02:46 +0900

    K번째 수 start

---
+++ Level_01/K번째수/index.js
@@ -0,0 +1,16 @@
+// Level 01 > 정렬 > K번째수
+
+function solution(array, commands) {
+  var answer = [];
+  return answer;
+}
+
+const result = solution(
+  [1, 5, 2, 6, 3, 7, 4],
+  [
+    [2, 5, 3],
+    [4, 4, 1],
+    [1, 7, 3],
+  ],
+);
+console.log("결과: ", result);
