d20250e0652b4bf493f9d486b9a61b10bd94ac5e
김효선 <kimhyosun@gimhyoseon-ui-MacBookPro.local>
2021-07-28 11:23:38 +0900

    K번째 수

--- Level_01/K번째수/index.js
+++ Level_01/K번째수/index.js
@@ -1,16 +1,28 @@
 // Level 01 > 정렬 > K번째수
 
+// <01>
 function solution(array, commands) {
-  var answer = [];
+  let answer = [];
+  let _array = [];
+
+  for (let i = 0; i < commands.length; i++) {
+    _array = array.slice(commands[i][0] - 1, commands[i][1]);
+    _array.sort((a, b) => a - b);
+    answer.push(_array[commands[i][2] - 1]);
+    _array = [];
+  }
+
   return answer;
 }
 
-const result = solution(
-  [1, 5, 2, 6, 3, 7, 4],
-  [
-    [2, 5, 3],
-    [4, 4, 1],
-    [1, 7, 3],
-  ],
-);
-console.log("결과: ", result);
+// <02>
+function solution2(array, commands) {
+  return commands.map((command) => {
+    const [start, end, pick] = command;
+    const newArray = array
+      .filter((_, index) => index >= start - 1 && index <= end - 1)
+      .sort((a, b) => a - b);
+
+    return newArray[pick - 1];
+  });
+}
