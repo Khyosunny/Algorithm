e2b08306c39c1c44d5c44c5afc92b5dbe8289776
Khyosunny <kimhyo_0218@naver.com>
2021-08-08 01:10:11 +0900

    프로그래머스 폴더 나누기

---
+++ programmers/Level_01/키패드 누르기/index.js
@@ -0,0 +1,9 @@
+// Level 01 > 2020 카카오 인턴십 > 키패드 누르기
+// <01>
+
+function solution(numbers, hand) {
+    var answer = '';
+    return answer;
+}
+
+console.log(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right"));
