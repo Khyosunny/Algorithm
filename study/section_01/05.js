d511a49830d9a1d8f61a321ee8301977e652d500
Khyosunny <kimhyo_0218@naver.com>
2021-08-08 22:07:43 +0900

    section_01 5번문제풀이

---
+++ study/section_01/05.js
@@ -0,0 +1,18 @@
+// section 01 >> 최솟값 구하기
+
+function solution(arr) {
+  let answer,
+    min = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+
+  answer = min;
+  return answer;
+}
+
+let arr = [5, 7, 1, 3, 2, 9, 11];
+console.log(solution(arr));
