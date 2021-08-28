5a8e16c5159504e77ae9e2a4cc4100c154275766
Khyosunny <kimhyo_0218@naver.com>
2021-08-28 20:43:39 +0900

    section_01 09번 문제풀이

---
+++ study/section_01/09.js
@@ -0,0 +1,17 @@
+// section 01 >> A를 #으로
+
+function solution(s) {
+  let answer = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "A") {
+      answer += "#";
+    } else {
+      answer += s[i];
+    }
+  }
+  return answer;
+}
+
+let str = "BANANA";
+console.log(solution(str));
