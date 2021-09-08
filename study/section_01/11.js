eb59d1e7b8405982cdefc163dd407c4ed8bef398
Khyosunny <kimhyo_0218@naver.com>
2021-09-08 23:59:32 +0900

    11

---
+++ study/section_01/11.js
@@ -0,0 +1,13 @@
+// section 01 >> 대문자 찾기
+
+function solution(s) {
+  let answer = 0;
+  const lower = s.toLowerCase();
+  console.log(lower);
+  // for (let x of s) {
+
+  // }
+}
+
+let str = 'KoreaTimeGood';
+console.log(solution(str));
