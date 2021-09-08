dde73fd4e4c47881cecacf6535239c7b76c237be
Khyosunny <kimhyo_0218@naver.com>
2021-09-09 00:25:18 +0900

    section_01 11번 문제풀이

--- study/section_01/11.js
+++ study/section_01/11.js
@@ -1,12 +1,26 @@
 // section 01 >> 대문자 찾기
 
+// 1)
 function solution(s) {
   let answer = 0;
   const lower = s.toLowerCase();
-  console.log(lower);
-  // for (let x of s) {
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== lower[i]) {
+      ++answer;
+    }
+  }
+  return answer;
+}
 
-  // }
+// 2)
+function solution2(s) {
+  let answer = 0;
+  for (let str of s) {
+    if (/[A-Z]/g.test(str)) {
+      ++answer;
+    }
+  }
+  return answer;
 }
 
 let str = 'KoreaTimeGood';
