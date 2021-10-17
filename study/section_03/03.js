6f1154e47aeb4445803c235ee83ea43e605d8b95
Khyosunny <kimhyo_0218@naver.com>
2021-10-17 19:13:23 +0900

    section_03 03번 문제풀이

---
+++ study/section_03/03.js
@@ -0,0 +1,14 @@
+// section 03 문자열 탐색 >> 숫자만 추출
+
+function solution(str) {
+  let answer = '';
+
+  for (let s of str) {
+    if (!isNaN(s)) answer += s;
+  }
+
+  return parseInt(answer);
+}
+
+let str = 'g0en2T0s8eSoft';
+console.log(solution(str));
