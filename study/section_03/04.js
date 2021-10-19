2a6ff8cd9b9ce9bfdd5692e05ea679c6c4c57759
Khyosunny <kimhyo_0218@naver.com>
2021-10-20 00:31:44 +0900

    section_03 04번 문제풀이

--- study/section_03/04.js
+++ study/section_03/04.js
@@ -2,9 +2,33 @@
 
 function solution(s, t) {
   let answer = [];
+  let count = 1000;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === t) {
+      count = 0;
+      answer.push(count);
+    } else {
+      answer.push(++count);
+    }
+  }
+  count = 1000;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === t) {
+      count = 0;
+    } else {
+      count++;
+      if (i === 0) {
+        console.log(answer[i], count);
+      }
+      answer[i] = Math.min(answer[i], count);
+    }
+  }
 
   return answer;
 }
 
-let str = 'teachermode';
+let str = 'tteachermode';
 console.log(solution(str, 'e'));
+
+// count의 초기값은 문자열의 길이보다 큰 값이어야한다.
