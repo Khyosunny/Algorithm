efbb43cb1c4f7f3557db31d2ae8cce970c9121d6
Khyosunny <kimhyo_0218@naver.com>
2021-10-23 00:57:55 +0900

    section_04 01번 문제풀이

--- study/section_04/01.js
+++ study/section_04/01.js
@@ -2,6 +2,22 @@
 
 function solution(n, arr) {
   let answer;
+  let max = Number.MIN_SAFE_INTEGER;
+
+  for (let i = 0; i < n; i++) {
+    let sum = 0,
+      tmp = arr[i];
+    while (tmp) {
+      sum += tmp % 10;
+      tmp = Math.floor(tmp / 10);
+    }
+    if (sum > max) {
+      max = sum;
+      answer = arr[i];
+    } else if (sum === max) {
+      if (arr[i] > answer) answer = arr[i];
+    }
+  }
 
   return answer;
 }
