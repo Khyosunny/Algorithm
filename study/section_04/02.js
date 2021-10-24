1ea33406177f87f7e34ea979f302418baca254b5
Khyosunny <kimhyo_0218@naver.com>
2021-10-25 00:20:27 +0900

    section_04 02번 문제풀이

--- study/section_04/02.js
+++ study/section_04/02.js
@@ -1,7 +1,26 @@
 // section 04 완전 탐색 >> 뒤집은 소수
 
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
 function solution(arr) {
-  let answer;
+  let answer = [];
+  for (let x of arr) {
+    let num = 0;
+    while (x) {
+      let t = x % 10;
+      num = num * 10 + t;
+      x = parseInt(x / 10);
+    }
+
+    if (isPrime(num)) answer.push(num);
+  }
+
   return answer;
 }
 
