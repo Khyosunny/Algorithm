3ff6afe6a98984268eb194bd307520bf50922b61
Khyosunny <kimhyo_0218@naver.com>
2021-10-27 23:46:15 +0900

    section_04 04번

---
+++ study/section_04/04.js
@@ -0,0 +1,15 @@
+// section 04 완전 탐색 >> 졸업 선물
+
+function solution(m, product) {
+  let answer;
+  return answer;
+}
+
+let arr = [
+  [6, 6],
+  [2, 2],
+  [4, 3],
+  [4, 5],
+  [10, 3],
+];
+console.log(solution(28, arr));
