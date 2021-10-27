db4ab0e62b931c1c72ae3e8e9eab0af8db2d61ca
Khyosunny <kimhyo_0218@naver.com>
2021-10-28 00:31:08 +0900

    section_04 04번 문제풀이

--- study/section_04/04.js
+++ study/section_04/04.js
@@ -1,7 +1,23 @@
 // section 04 완전 탐색 >> 졸업 선물
 
 function solution(m, product) {
-  let answer;
+  let answer = 0;
+
+  const students = product.length;
+  product.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
+  for (let i = 0; i < students; i++) {
+    let money = m - (product[i][0] / 2 + product[i][1]);
+    let count = 1;
+    for (let j = 0; j < students; j++) {
+      if (j !== i && product[j][0] + product[j][1] > money) break;
+      if (j !== i && product[j][0] + product[j][1] <= money) {
+        money -= product[j][0] + product[j][1];
+        count++;
+      }
+    }
+    answer = Math.max(answer, count);
+  }
+
   return answer;
 }
 
