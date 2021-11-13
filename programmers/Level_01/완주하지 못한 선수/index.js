828b01446d2d31b3d88a02508a67a958c769e9c4
Khyosunny <kimhyo_0218@naver.com>
2021-11-13 15:57:20 +0900

    완주하지 못한 선수 Map으로 풀이 추가

--- programmers/Level_01/완주하지 못한 선수/index.js
+++ programmers/Level_01/완주하지 못한 선수/index.js
@@ -1,7 +1,7 @@
 // Level 01 > 해시 > 완주하지 못한 선수
 // <01>
 function solution(participant, completion) {
-  let answer = "";
+  let answer = '';
   participant.sort();
   completion.sort();
 
@@ -31,3 +31,22 @@ function solution2(participant, completion) {
     }
   }
 }
+
+// <03> Map으로 풀이
+function solution3(participant, completion) {
+  const hash = new Map();
+
+  for (let i = 0; i < participant.length; i++) {
+    hash.set(participant[i], (hash.get(participant[i]) || 0) + 1);
+    hash.set(completion[i], (hash.get(completion[i]) || 0) - 1);
+    console.log(hash);
+  }
+
+  for (let [key, val] of hash) {
+    if (val > 0) return key;
+  }
+}
+
+console.log(
+  solution3(['mislav', 'stanko', 'mislav', 'ana'], ['stanko', 'ana', 'mislav'])
+);
